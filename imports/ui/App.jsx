72f529c12415087336d7323a73f7945588a14a85
minipolarbear1 <jy.kim@glnt.co.kr>
2023-01-16 15:34:16 +0900

    first commit

---
+++ imports/ui/App.jsx
@@ -0,0 +1,105 @@
+import React, {Fragment, useState} from 'react';
+import { useTracker } from 'meteor/react-meteor-data';
+import { TasksCollection } from '../api/TasksCollection';
+import { Task } from './Task'
+import {TaskForm} from "./TaskForm";
+import {LoginForm} from "./LoginForm";
+
+
+const toggleChecked = ({ _id, isChecked}) => {
+    TasksCollection.update(_id, {
+        $set: {
+            isChecked: !isChecked
+        }
+    })
+};
+export const App = () => {
+    const user = useTracker(() => Meteor.user());
+
+    const [hideCompleted, setHideCompleted] = useState(false);
+    const [taskUpdate, setTaskUpdate] = useState(false);
+
+    const hideCompletedFilter = { isChecked: {$ne: true}}
+
+    const userFilter = user ? { userId: user._id } : {};
+
+    /*
+    ...(Three dots) es6에서 새로 추가된 문법
+    1. Three dots 함수가 매개변수의 끝에 있으면 "나머지 매개 변수" 이고 나머지 인수 목록을 배열로 수집
+    2. 함수 호출 등에서 Three dots 사용하면 "확산 연산자" 라고 하며 배열을 목록으로 확장
+    참고 사이트
+    https://godnr149.tistory.com/150
+    https://dinn.github.io/javascript/js-dotdotdot/
+    */
+    const pendingOnlyFilter = { ...hideCompletedFilter, ...userFilter };
+    //최신순으로 정렬
+    //sort 1: ASC, -1: DESC
+    const tasks = useTracker(() =>{
+        if(!user){
+            return [];
+        }
+
+        return TasksCollection.find(hideCompleted ? pendingOnlyFilter : userFilter, {
+            sort: { createdAt: -1 },
+        }).fetch();
+    });
+
+    //테스크 삭제
+    const deleteTask = ({ _id}) => TasksCollection.remove(_id);
+
+    const pendingTasksCount = useTracker(() => {
+        if (!user) {
+            return 0;
+        }
+        return TasksCollection.find(pendingOnlyFilter).count();
+    });
+
+    const pendingTasksTitle = `${
+       pendingTasksCount ? `(${pendingTasksCount})` : '' 
+    }`;
+    const logout = () => Meteor.logout();
+
+    return (
+        <div className="app">
+            <header>
+                <div className="app-bar">
+                    <div className="app-header">
+                        <h1>📝️ To Do List</h1>
+                        {pendingTasksTitle}
+                    </div>
+                </div>
+            </header>
+
+            <div className="main">
+                {user ?(
+                    <Fragment>
+                        <div className="user" onClick={logout}>
+                            {user.username}
+                        </div>
+
+                        <TaskForm user={user} />
+
+                        <div className="filter">
+                            <button onClick={() => setHideCompleted(!hideCompleted)}>
+                                {hideCompleted? 'Show All' : 'Hide Completed'}
+                            </button>
+                        </div>
+
+                        <ul className="tasks">
+                            {tasks.map(task => (
+                                <Task
+                                    key={task._id}
+                                    task={task}
+                                    onCheckboxClick={toggleChecked}
+                                    onDeleteClick={deleteTask}
+                                />
+                            ))}
+                        </ul>
+                    </Fragment>
+                ) : (
+                    <LoginForm/>
+                )}
+            </div>
+        </div>
+    );
+};
